d479c8c0492be617e33a35a1e4fa4e274f2629a6
silvia <frcuenca05@gmail.com>
2020-03-18 22:34:35 +0100

    Refactor problem 1

--- problem1/Solution.py
+++ problem1/Solution.py
@@ -1,26 +1,11 @@
-def find_four(s):
-    try:
-        idx = s.index('4')
-    except:
-        idx = -1
-
-    return idx
-
-
 def main():
     t = int(input())
 
     for i in range(1, t+1):
         n = input()
-        a = list(n)
 
-        idx = find_four(a)
+        a = n.replace('4', '2')
 
-        while idx > -1:
-            a[idx] = 2
-            idx = find_four(a)
-
-        a = ''.join(map(str, a))
         b = int(n) - int(a)
         print('Case #{0}: {1} {2}'.format(i, a, b))
 
